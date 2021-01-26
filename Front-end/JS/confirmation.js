32c344c9e1da5f377406df68bb3518eb75ba2392
christopher guillou <christopher.guilou@outlook.fr>
2021-01-26 13:56:09 +0100

    creation fichier js et html

---
+++ Front-end/JS/confirmation.js
@@ -0,0 +1,5 @@
+/*
+    une page de confirmation de commande, remerciant l'utilisateur pour sa
+    commande, et indiquant le prix total et l'identifiant de commande envoyé
+    par le serveur.
+*/
---
+++ Front-end/JS/view.js
@@ -0,0 +1,4 @@
+/*
+    une page de vue sous forme de liste, montrant tous les articles disponibles
+    à la vente ;
+*/
